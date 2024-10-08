0b3afe93b31459f3c130caf649158df9565fafc8
Kauanoe <ajafaith@hawaii.edu>
2024-10-08 12:11:52 -1000

    week 06 homework upload :)

---
+++ Week_06/Script/Week_06A_script.R
@@ -0,0 +1,12 @@
+
+### INTRODUCTION ###
+# project: Week_06A RMarkdown 
+# name: Kauanoe Greene
+# updated on: 2024-10-07
+
+
+# PACKAGES
+install.packages("rmarkdown") # install the stuffs
+install.packages('tinytex') # this will be helpful when making a PDF.
+
+
